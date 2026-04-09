837d626cd8f711bff2145990960dcc5cd46d3097
Joshua GonSan <canary_styl3@hotmail.com>
2026-04-09 19:19:01 +0200

    Update __manifest__.py

--- datetime_countdown/__manifest__.py
+++ datetime_countdown/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Datetime Countdown Widget (Odoo 18)",
+    "name": "Datetime Countdown Widget",
     "summary": "Human-friendly datetime countdown field widget",
     "version": "18.0.1.0.0",
     "category": "Tools",
