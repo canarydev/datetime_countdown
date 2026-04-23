c05cf55e535ad5c400e13a893ff8ebce637a2925
Joshua GonSan <canary_styl3@hotmail.com>
2026-04-23 20:15:09 +0200

    Update __manifest__.py

--- datetime_countdown/__manifest__.py
+++ datetime_countdown/__manifest__.py
@@ -4,7 +4,7 @@
     "version": "16.0.1.0.0",
     "category": "Tools",
     "license": "LGPL-3",
-    "author": "CanaryDev",
+    "author": "canarydev",
     "website": "https://canarydev.es",
     "depends": ["web"],
     'images': ['static/description/banner.png'],
